79cc1569a7b9b73db883bdec6b8d74bf41037f26
DESKTOP-E8L7QGR\hmy <mihe4117@uni.sydney.edu.au>
2018-08-24 18:33:31 +1000

    project ui initialised

---
+++ Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/DrawActivity.java
@@ -0,0 +1,13 @@
+package com.project3400.usyd.handwritingcapturing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+    }
+}
---
+++ Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/MainActivity.java
@@ -0,0 +1,41 @@
+package com.project3400.usyd.handwritingcapturing;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ImageButton imgbtn1 = (ImageButton) findViewById(R.id.imgbtn1);
+        imgbtn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DrawActivity.class));
+            }
+        });
+
+        ImageButton imgbtn2 = (ImageButton) findViewById(R.id.imgbtn2);
+        imgbtn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DrawActivity.class));
+            }
+        });
+
+        ImageButton imgbtn3 = (ImageButton) findViewById(R.id.imgbtn3);
+        imgbtn3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DrawActivity.class));
+            }
+        });
+
+
+
+    }
+}
