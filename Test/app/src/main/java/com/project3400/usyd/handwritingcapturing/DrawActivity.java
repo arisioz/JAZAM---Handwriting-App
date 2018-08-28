9273bc572f9b2c6c840974e63bfdd5e498cfedbe
DESKTOP-E8L7QGR\hmy <mihe4117@uni.sydney.edu.au>
2018-08-28 14:30:54 +1000

    return transparerncy fixed

--- Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/DrawActivity.java
+++ Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/DrawActivity.java
@@ -1,7 +1,10 @@
 package com.project3400.usyd.handwritingcapturing;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DrawActivity extends Activity {
 
@@ -9,5 +12,19 @@ public class DrawActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
+
+
+        ImageButton imgbtn_back = (ImageButton) findViewById(R.id.imgbtn_back);
+        imgbtn_back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(DrawActivity.this, MainActivity.class));
+            }
+        });
+
     }
+
+
+
+
+
 }
