8c73d55892c6cd6e9ba50fc2bcfec19d60a7c343
DESKTOP-E8L7QGR\hmy <mihe4117@uni.sydney.edu.au>
2018-09-01 18:17:51 +1000

    add random

--- Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/MainActivity.java
+++ Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.Random;
+
 public class MainActivity extends Activity {
 
     public static int chosenShape;
@@ -15,10 +17,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final int shapes[] = {R.drawable.triangle,R.drawable.circle,R.drawable.square};
+
         ImageButton imgbtn1 = (ImageButton) findViewById(R.id.imgbtn1);
         imgbtn1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                chosenShape = R.drawable.triangle;
+                chosenShape = shapes[0];
                 startActivity(new Intent(MainActivity.this, DrawActivity.class));
             }
         });
@@ -26,7 +30,7 @@ public class MainActivity extends Activity {
         ImageButton imgbtn2 = (ImageButton) findViewById(R.id.imgbtn2);
         imgbtn2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                chosenShape = R.drawable.circle;
+                chosenShape = shapes[1];
                 startActivity(new Intent(MainActivity.this, DrawActivity.class));
             }
         });
@@ -34,10 +38,20 @@ public class MainActivity extends Activity {
         ImageButton imgbtn3 = (ImageButton) findViewById(R.id.imgbtn3);
         imgbtn3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                chosenShape = R.drawable.square;
+                chosenShape = shapes[2];
                 startActivity(new Intent(MainActivity.this, DrawActivity.class));
             }
         });
+
+
+        ImageButton imgbtnRd = (ImageButton) findViewById(R.id.imgbtnRd);
+        imgbtnRd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                chosenShape = shapes[new Random().nextInt(shapes.length)];
+                startActivity(new Intent(MainActivity.this, DrawActivity.class));
+            }
+        });
+
 
 
 
