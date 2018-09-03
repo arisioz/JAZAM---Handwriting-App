49bdd9c134839a92c66006fc33e7e3254b377d21
Ariel <siozaris@outlook.com>
2018-09-04 00:42:55 +1000

    Code Readability, A more optimized way for the buttons, Removed back button and used the android hierarchical builtin back option. Also don't remove the comments as I'm trying to make it even more optimized and less code redundant

--- Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/DrawActivity.java
+++ Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/DrawActivity.java
@@ -3,8 +3,6 @@ package com.project3400.usyd.handwritingcapturing;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class DrawActivity extends Activity {
@@ -14,19 +12,10 @@ public class DrawActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
 
+        //Intent activity_Main_To_Draw = getIntent();
+        //int source = activity_Main_To_Draw.getIntExtra("chosen_Shape", 2130968584);
+
         ImageView img = (ImageView) findViewById(R.id.imgdraw);
-        img.setImageResource(MainActivity.chosenShape);
-
-
-
-        ImageButton imgbtn_back = (ImageButton) findViewById(R.id.imgbtn_back);
-        imgbtn_back.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(DrawActivity.this, MainActivity.class));
-            }
-        });
-
+        img.setImageResource(MainActivity.chosen_Shape);
     }
-
-
 }
--- Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/MainActivity.java
+++ Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/MainActivity.java
@@ -10,50 +10,55 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
 
-    public static int chosenShape;
+    /**
+     * Class variables followed by the instance variables
+     */
+    public static int chosen_Shape;
+    private ImageButton image_Button_1;
+    private ImageButton image_Button_2;
+    private ImageButton image_Button_3;
+    private ImageButton image_Button_Random;
+    private final int possible_Shapes[] = {R.drawable.triangle,R.drawable.circle,R.drawable.square};
+
+    private View.OnClickListener tweakedOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId())
+            {
+                case R.id.imgbtn1:
+                    chosen_Shape = possible_Shapes[0];
+                    break;
+                case R.id.imgbtn2:
+                    chosen_Shape = possible_Shapes[1];
+                    break;
+                case R.id.imgbtn3:
+                    chosen_Shape = possible_Shapes[2];
+                    break;
+                case R.id.imgbtnRd:
+                    int random_Number = new Random().nextInt(possible_Shapes.length);
+                    chosen_Shape = possible_Shapes[random_Number];
+                default:
+                    throw new RuntimeException("Unknown button ID");
+            }
+            Intent activity_Main_To_Draw = new Intent(MainActivity.this, DrawActivity.class);
+            //activity_Main_To_Draw.putExtra("chosen_Shape",v.getId());
+            startActivity(activity_Main_To_Draw);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final int shapes[] = {R.drawable.triangle,R.drawable.circle,R.drawable.square};
+        image_Button_1 = findViewById(R.id.imgbtn1);
+        image_Button_2 = findViewById(R.id.imgbtn2);
+        image_Button_3 = findViewById(R.id.imgbtn3);
+        image_Button_Random = findViewById(R.id.imgbtnRd);
 
-        ImageButton imgbtn1 = (ImageButton) findViewById(R.id.imgbtn1);
-        imgbtn1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                chosenShape = shapes[0];
-                startActivity(new Intent(MainActivity.this, DrawActivity.class));
-            }
-        });
-
-        ImageButton imgbtn2 = (ImageButton) findViewById(R.id.imgbtn2);
-        imgbtn2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                chosenShape = shapes[1];
-                startActivity(new Intent(MainActivity.this, DrawActivity.class));
-            }
-        });
-
-        ImageButton imgbtn3 = (ImageButton) findViewById(R.id.imgbtn3);
-        imgbtn3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                chosenShape = shapes[2];
-                startActivity(new Intent(MainActivity.this, DrawActivity.class));
-            }
-        });
-
-
-        ImageButton imgbtnRd = (ImageButton) findViewById(R.id.imgbtnRd);
-        imgbtnRd.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                chosenShape = shapes[new Random().nextInt(shapes.length)];
-                startActivity(new Intent(MainActivity.this, DrawActivity.class));
-            }
-        });
-
-
-
-
+        image_Button_1.setOnClickListener(tweakedOnClickListener);
+        image_Button_2.setOnClickListener(tweakedOnClickListener);
+        image_Button_3.setOnClickListener(tweakedOnClickListener);
+        image_Button_Random.setOnClickListener(tweakedOnClickListener);
     }
 }
