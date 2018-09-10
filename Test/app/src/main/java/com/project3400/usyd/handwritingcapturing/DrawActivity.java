71dccc03138eb5a551abc50591df6c7b39f2ea2f
DESKTOP-E8L7QGR\hmy <mihe4117@uni.sydney.edu.au>
2018-09-11 00:29:00 +1000

    add ontouch listener

--- Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/DrawActivity.java
+++ Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/DrawActivity.java
@@ -1,11 +1,14 @@
 package com.project3400.usyd.handwritingcapturing;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
-
 public class DrawActivity extends Activity {
+
+    private ImageView img;
+    private ImageView tracePad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,39 @@ public class DrawActivity extends Activity {
         //Intent activity_Main_To_Draw = getIntent();
         //int source = activity_Main_To_Draw.getIntExtra("chosen_Shape", 2130968584);
 
-        ImageView img = (ImageView) findViewById(R.id.imgdraw);
+        img = findViewById(R.id.imgdraw);
         img.setImageResource(MainActivity.chosen_Shape);
+
+        tracePad = findViewById(R.id.tracepad);
+        tracePad.setOnTouchListener(handleTouch);
+
     }
+
+
+    private View.OnTouchListener handleTouch = new View.OnTouchListener() {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    System.out.println("touch down");
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    System.out.println( "moving: (" + x + ", " + y + ")");
+                    break;
+                case MotionEvent.ACTION_UP:
+                    System.out.println("touch up");
+                    break;
+            }
+
+            return true;
+        }
+
+
+    };
+
 }
--- Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/MainActivity.java
+++ Test/app/src/main/java/com/project3400/usyd/handwritingcapturing/MainActivity.java
@@ -47,6 +47,10 @@ public class MainActivity extends Activity {
         }
     };
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
